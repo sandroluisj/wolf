27c287b8132cbb780811508f2144d0e3ef3e68af
Sheldon441 <sheldonmarcos021@gmail.com>
2024-07-29 15:23:24 -0300

    Sheldon 4-numerosprimos.js

--- 4-numerosprimos.js
+++ 4-numerosprimos.js
@@ -8,8 +8,21 @@ se ele é divisível por qualquer número entre 2 e a raiz quadrada do número.
 Você pode usar a função sqrt() da biblioteca Math para calcular isso.
 
 Programa desenvolvido por:
- - Aluno 1
- - Aluno 2
- - Aluno 3
- - Aluno 4
+ - Sheldon
+ - Sandro
+ - Suhaila
+ - Kauany
 */
+    
+let array = [1,2,3,4,5,6,7,8,9,10]
+    function isPrime(number){
+      for (let i = 2; number > i; i++){
+        if (number % i === 0) {
+            return false;
+        }
+}
+    return number > 1;
+}
+
+console.log (`${array.filter(isPrime)}`);
+console.log(isPrime(5))

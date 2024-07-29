a58b6da6104006cdfaabfedc8585ef34be81e431
Suhaila Orba Abib <suhailaorbaabib@gmail.com>
2024-07-29 14:53:56 -0300

    Update 1-maiornumero.js

--- 1-maiornumero.js
+++ 1-maiornumero.js
@@ -3,8 +3,20 @@ Crie uma função chamada findMax que aceita um array de números e retorne
 o maior número do array.
 
 Programa desenvolvido por:
- - Aluno 1
- - Aluno 2
- - Aluno 3
- - Aluno 4
+ - Suháila
+ - Sheldon
+ - Kauane
+ - Sandro
 */
+
+function findMax(vetor){
+  let maior = vetor[0]
+    for(let i = 0; i < vetor.length; i++){
+      if(vetor[i] > maior){
+       maior = vetor[i]
+   }
+}
+    return maior
+
+}
+    console.log(findMax([ 2, 53, 36, 98]))
